7ee4e24520bbfceadf3ba53035a8c11dfe56fe49
Orb_H <orb.cov@gmail.com>
2020-05-13 11:44:49 +0900

    add shortcuts in interpreter(1.2.0)

--- _docs/extras/interpreter.md
+++ _docs/extras/interpreter.md
@@ -5,7 +5,7 @@ tags:
     - code
 ---
 
-# 인터프리터 v1.1.0
+# 인터프리터 v1.2.0
 
 Probie 코드를 실행해볼 수 있는 페이지입니다. 코드 칸과 출력 칸은 크기를 마음대로 바꿀 수 있습니다.
 
@@ -16,8 +16,29 @@ Probie 코드를 실행해볼 수 있는 페이지입니다. 코드 칸과 출�
 <textarea style="resize:both;font-family:'Monospace';background-color:white;" id="output" disabled="true"></textarea>
 
 <button onclick="run()">실행</button>
-(실행 시간:
-<span id="time"></span> ms)
+(실행 시간: <span id="time"></span> ms)
+
+---
+
+**기능 단축키**
+
+|기능|단축키|
+|:-:|:-:|
+|코드 실행|<kbd>Alt</kbd> + <kbd>Enter</kbd>|
+
+**특수기호 단축키**
+
+|기호|단축키|
+|:-:|:-:|
+|<kbd>↑</kbd> / <kbd>←</kbd> / <kbd>↓</kbd> / <kbd>→</kbd>|<kbd>Alt</kbd> + (<kbd>W</kbd> / <kbd>A</kbd> / <kbd>S</kbd> / <kbd>D</kbd>)|
+|<kbd>△</kbd> / <kbd>◁</kbd> / <kbd>▽</kbd> / <kbd>▷</kbd>|<kbd>Alt</kbd> + (<kbd>I</kbd> / <kbd>J</kbd> / <kbd>K</kbd> / <kbd>L</kbd>)|
+|<kbd>▲</kbd> / <kbd>◀</kbd> / <kbd>▼</kbd> / <kbd>▶</kbd>|<kbd>Alt</kbd> + (<kbd>T</kbd> / <kbd>F</kbd> / <kbd>G</kbd> / <kbd>H</kbd>)|
+|<kbd>↔</kbd> / <kbd>↕</kbd>|<kbd>Alt</kbd> + (<kbd>Z</kbd> / <kbd>X</kbd>)|
+|<kbd>×</kbd> / <kbd>÷</kbd>|<kbd>Alt</kbd> + (<kbd>M</kbd> / <kbd>N</kbd>)|
+|<kbd>∨</kbd> / <kbd>∧</kbd>|<kbd>Alt</kbd> + (<kbd>V</kbd> / <kbd>B</kbd>)|
+|값에 해당하는 문자|<sup>※</sup>hex code + <kbd>Alt</kbd> + <kbd>C</kbd>|
+
+※ hex code를 굳이 입력하지 않아도 hex code 뒤쪽으로 커서를 옮긴 후 Alt + C를 입력해도 코드 변환이 가능합니다.
 
 <script src="{{ site.baseurl }}/assets/js/probie_short.min.js"></script>
 <script src="{{ site.baseurl }}/assets/js/interpreter.js"></script>
--- assets/js/interpreter.js
+++ assets/js/interpreter.js
@@ -1,3 +1,7 @@
+document
+    .getElementById("code")
+    .addEventListener("keydown", code_mnemonic, false);
+
 var p = undefined;
 
 function run() {
@@ -22,3 +26,93 @@ var stderrcb = function (c, text) {
 var stdcodecb = function (c) {
     document.getElementById("code").value = c.join("\n");
 };
+
+function code_mnemonic(event) {
+    var c = document.getElementById("code");
+    var s = c.selectionStart; //location of start cursor
+    var e = c.selectionEnd; //location of end cursor
+    var r = ""; //converted string
+    var a = s + 1; //cursor location after conversion
+    if (event.altKey) {
+        var b = true; //manipulate code textarea?
+        event.preventDefault();
+        switch (event.keyCode) {
+            case 0x57: //W
+                r = c.value.substring(0, s) + "↑" + c.value.substring(e);
+                break;
+            case 0x41: //A
+                r = c.value.substring(0, s) + "←" + c.value.substring(e);
+                break;
+            case 0x53: //S
+                r = c.value.substring(0, s) + "↓" + c.value.substring(e);
+                break;
+            case 0x44: //D
+                r = c.value.substring(0, s) + "→" + c.value.substring(e);
+                break;
+            case 0x49: //I
+                r = c.value.substring(0, s) + "△" + c.value.substring(e);
+                break;
+            case 0x4a: //J
+                r = c.value.substring(0, s) + "◁" + c.value.substring(e);
+                break;
+            case 0x4b: //K
+                r = c.value.substring(0, s) + "▽" + c.value.substring(e);
+                break;
+            case 0x4c: //L
+                r = c.value.substring(0, s) + "▷" + c.value.substring(e);
+                break;
+            case 0x54: //T
+                r = c.value.substring(0, s) + "▲" + c.value.substring(e);
+                break;
+            case 0x46: //F
+                r = c.value.substring(0, s) + "◀" + c.value.substring(e);
+                break;
+            case 0x47: //G
+                r = c.value.substring(0, s) + "▼" + c.value.substring(e);
+                break;
+            case 0x48: //H
+                r = c.value.substring(0, s) + "▶" + c.value.substring(e);
+                break;
+            case 0x5a: //Z
+                r = c.value.substring(0, s) + "↔" + c.value.substring(e);
+                break;
+            case 0x58: //X
+                r = c.value.substring(0, s) + "↕" + c.value.substring(e);
+                break;
+            case 0x4d: //M
+                r = c.value.substring(0, s) + "×" + c.value.substring(e);
+                break;
+            case 0x4e: //N
+                r = c.value.substring(0, s) + "÷" + c.value.substring(e);
+                break;
+            case 0x56: //V
+                r = c.value.substring(0, s) + "∨" + c.value.substring(e);
+                break;
+            case 0x42: //B
+                r = c.value.substring(0, s) + "∧" + c.value.substring(e);
+                break;
+            case 0x43: //C
+                var n = parseInt(c.value.substring(e - 2, e), 16);
+                if (n >= 0 && n <= 0x7f) {
+                    r =
+                        c.value.substring(0, e - 2) +
+                        $P.Y[n] +
+                        c.value.substring(e);
+                    a = e - 1;
+                } else {
+                    b = false;
+                }
+                break;
+            case 0xd: //Enter
+                b = false;
+                run();
+                break;
+            default:
+                b = false;
+        }
+        if (b) {
+            c.value = r;
+            c.selectionStart = c.selectionEnd = a;
+        }
+    }
+}
